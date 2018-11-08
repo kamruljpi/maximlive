15d08b2e65868fabfe6e231107bbce93802b59c9
shishir <shishir@bookhint.com>
2018-11-08 18:44:37 +0600

    Supplier New Field

--- project/app/Supplier.php
+++ project/app/Supplier.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Supplier extends Model
 {
     protected $primaryKey = 'supplier_id';
-    protected $fillable = ['supplier_id', 'name', 'phone', 'address', 'status'];
+    protected $fillable = ['supplier_id', 'name', 'phone', 'person_name','email', 'address', 'status'];
 }

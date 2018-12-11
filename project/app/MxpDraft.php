f898af2848678aa03d86f1092bd084da1cdf2dff
shishir <shishir@bookhint.com>
2018-12-11 14:30:33 +0600

    Model added

---
+++ project/app/MxpDraft.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class mxpDraft extends Model
+{
+    protected $table = 'mxp_draft';
+    
+    protected $fillable = [
+    			'user_id',
+    			'booking_order_id',
+    			'erp_code',
+    			'item_code',
+    			'item_size',
+    			'item_quantity',
+    			'item_price',
+    			'matarial',
+    			'gmts_color',
+    			'orderDate',
+    			'orderNo',
+                'shipmentDate',
+    			'sku',
+    			'poCatNo',
+                'oos_number',
+                'season_code'];
+}
